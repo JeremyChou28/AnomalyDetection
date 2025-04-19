6fc907e126f64c61186822f66b1c60b287f67aaf
JeremyChou28 <jeremychou28@gmail.com>
2025-04-19 11:00:31 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Anomaly Detection for Zhenyu Chen
+
+## Requirements
+
+```shell
+pip install -r requirements.txt
+```
+
+## Datasets
+
+Download datasets and put on `processed_datasets/`
+
+## How to run
+
+`bash scripts/run.sh`
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,19 @@
 export CUDA_VISIBLE_DEVICES=3
 
-# nohup python -u main.py \
-#   --anormly_ratio 0.5 \
-#   --num_epochs 100 \
-#   --batch_size 256 \
-#   --mode train \
-#   --dataset AD \
-#   --data_path ./processed_dataset \
-#   --input_c 1 > logs/AD_train.log 2>&1 &
-  
 
+# training
+nohup python -u main.py \
+  --anormly_ratio 0.5 \
+  --num_epochs 100 \
+  --batch_size 256 \
+  --mode train \
+  --dataset AD \
+  --data_path ./processed_dataset \
+  --input_c 1 > logs/AD_train.log 2>&1 &
+
+wait  
+
+# testing
 nohup python -u main.py \
   --anormly_ratio 0.5 \
   --num_epochs 100 \
